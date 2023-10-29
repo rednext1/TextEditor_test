bd900f0de54de8a8cdc067fa9ca5a06f8ab23285
Yan <none@none.com>
2023-10-29 22:51:41 +0200

    Revert "ddsfdsfdfsf"
    This reverts commit e16a31c2d68751f739adbaf0fc575956d3c17d85.

--- TextEditor/Form1.cs
+++ TextEditor/Form1.cs
@@ -5,7 +5,6 @@ namespace TextEditor
         public Main_Form()
         {
             InitializeComponent();
-            //ssss
         }
 
         private void Main_Form_FormClosing(object sender, FormClosingEventArgs e)
